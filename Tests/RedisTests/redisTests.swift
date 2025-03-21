51c9b1c9864504006c356a25d6d92774f8b120c9
Adam Fowler <adamfowler71@gmail.com>
2025-03-21 10:58:51 +0000

    Add set/get test

--- Tests/RedisTests/redisTests.swift
+++ Tests/RedisTests/redisTests.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Logging
 import Testing
 
@@ -7,7 +8,9 @@ import Testing
     var logger = Logger(label: "Redis")
     logger.logLevel = .debug
     try await RedisClient.connect(.hostname("localhost", port: 6379), logger: logger) { connection, logger in
-        let response = try await connection.send(.init("GET", arguments: ["ping"]))
-        print(String(from: response) ?? "DIDNT CONVERT")
+        let key = RedisKey(rawValue: UUID().uuidString)
+        _ = try await connection.setex(key: key, seconds: 50, value: "Hello")
+        let response = try await connection.get(key: key)
+        #expect(String(from: response) == "Hello")
     }
 }
