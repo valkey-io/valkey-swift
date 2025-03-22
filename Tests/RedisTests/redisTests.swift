103a5e75a5855ecae4f9fb13b2551bd0bfd24702
Adam Fowler <adamfowler71@gmail.com>
2025-03-22 17:07:01 +0000

    Add converting(to:) to ease conversion

--- Tests/RedisTests/redisTests.swift
+++ Tests/RedisTests/redisTests.swift
@@ -5,48 +5,52 @@ import Testing
 
 @testable import Redis
 
-@Test func testSetGet() async throws {
-    var logger = Logger(label: "Redis")
-    logger.logLevel = .debug
-    try await RedisClient.withConnection(.hostname("localhost", port: 6379), logger: logger) { connection, logger in
-        let key = RedisKey(rawValue: UUID().uuidString)
-        _ = try await connection.set(key: key, value: "Hello")
-        let response = try await connection.get(key: key)
-        #expect(try String(from: response) == "Hello")
+struct GeneratedCommands {
+    @Test
+    func testSetGet() async throws {
+        var logger = Logger(label: "Redis")
+        logger.logLevel = .debug
+        try await RedisClient.withConnection(.hostname("localhost", port: 6379), logger: logger) { connection, logger in
+            let key = RedisKey(rawValue: UUID().uuidString)
+            _ = try await connection.set(key: key, value: "Hello")
+            let response = try await connection.get(key: key).converting(to: String.self)
+            #expect(response == "Hello")
+        }
     }
-}
 
-@Test func testSort() async throws {
-    var logger = Logger(label: "Redis")
-    logger.logLevel = .debug
-    try await RedisClient.withConnection(.hostname("localhost", port: 6379), logger: logger) { connection, logger in
-        let key = RedisKey(rawValue: UUID().uuidString)
-        _ = try await connection.lpush(key: key, element: "a")
-        _ = try await connection.lpush(key: key, element: "c")
-        _ = try await connection.lpush(key: key, element: "b")
-        let list = try await connection.sort(key: key, sorting: true)
-        let array = try [String](from: list)
-        #expect(array == ["a", "b", "c"])
+    @Test
+    func testSort() async throws {
+        var logger = Logger(label: "Redis")
+        logger.logLevel = .debug
+        try await RedisClient.withConnection(.hostname("localhost", port: 6379), logger: logger) { connection, logger in
+            let key = RedisKey(rawValue: UUID().uuidString)
+            _ = try await connection.lpush(key: key, element: "a")
+            _ = try await connection.lpush(key: key, element: "c")
+            _ = try await connection.lpush(key: key, element: "b")
+            let list = try await connection.sort(key: key, sorting: true).converting(to: [String].self)
+            #expect(list == ["a", "b", "c"])
+        }
     }
-}
 
-@Test func testLMPOP() async throws {
-    var logger = Logger(label: "Redis")
-    logger.logLevel = .debug
-    try await RedisClient.withConnection(.hostname("localhost", port: 6379), logger: logger) { connection, logger in
-        let key = RedisKey(rawValue: UUID().uuidString)
-        let key2 = RedisKey(rawValue: UUID().uuidString)
-        _ = try await connection.lpush(key: key, element: "a")
-        _ = try await connection.lpush(key: key2, element: "b")
-        let rt1 = try await [RESP3Token](from: connection.lmpop(key: key, key2, where: .left))
-        let keyReturned1 = try RedisKey(from: rt1[0])
-        let values1 = try [String](from: rt1[1])
-        #expect(keyReturned1 == key)
-        #expect(values1.first == "a")
-        let rt2 = try await [RESP3Token](from: connection.lmpop(key: key, key2, where: .left))
-        let keyReturned2 = try RedisKey(from: rt2[0])
-        let values2 = try [String](from: rt2[1])
-        #expect(keyReturned2 == key2)
-        #expect(values2.first == "b")
+    @Test("Array with count using LMPOP")
+    func testArrayWithCount() async throws {
+        var logger = Logger(label: "Redis")
+        logger.logLevel = .debug
+        try await RedisClient.withConnection(.hostname("localhost", port: 6379), logger: logger) { connection, logger in
+            let key = RedisKey(rawValue: UUID().uuidString)
+            let key2 = RedisKey(rawValue: UUID().uuidString)
+            _ = try await connection.lpush(key: key, element: "a")
+            _ = try await connection.lpush(key: key2, element: "b")
+            let rt1 = try await connection.lmpop(key: key, key2, where: .left).converting(to: [RESP3Token].self)
+            let keyReturned1 = try RedisKey(from: rt1[0])
+            let values1 = try [String](from: rt1[1])
+            #expect(keyReturned1 == key)
+            #expect(values1.first == "a")
+            let rt2 = try await connection.lmpop(key: key, key2, where: .left).converting(to: [RESP3Token].self)
+            let keyReturned2 = try RedisKey(from: rt2[0])
+            let values2 = try [String](from: rt2[1])
+            #expect(keyReturned2 == key2)
+            #expect(values2.first == "b")
+        }
     }
 }
