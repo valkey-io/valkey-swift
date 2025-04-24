ad30309beb7a40dac9991dbfa17db85222443214
Adam Fowler <adamfowler71@gmail.com>
2025-04-24 15:13:13 +0100

    Subscriptions (#21)
    * Hacky subscriptions
    
    * Subscriptions using a closure
    
    * Re-implemented subscriptions
    
    * Ensure we cleanup after an error and add tests
    
    * Fix error when handling unsubscribe errors
    
    * Test everything is cleaned up
    
    * ValkeySubscriptionSequence, comments, `@inlinable`
    
    * Add SSUBSCRIBE support
    
    * Added state machine for each channel
    
    * Add channel state machine tests
    
    * Add isolated parameter to subscribe functions
    
    * Use ValkeyPromise for subscribe commands
    
    * Collapse token type into subscribe, unsubscribe and message
    
    * PR comments
    
    * Add some comments
    
    * Update Sources/Valkey/Connection/ValkeyChannelHandler.swift
    
    Co-authored-by: Fabian Fett <fabianfett@apple.com>
    
    * Update Sources/Valkey/Connection/ValkeyChannelHandler.swift
    
    Co-authored-by: Fabian Fett <fabianfett@apple.com>
    
    * Update Sources/Valkey/Connection/ValkeyChannelHandler.swift
    
    Co-authored-by: Fabian Fett <fabianfett@apple.com>
    
    * Tidy up received push token trace log, removed message from output
    
    * Add isolation to lower level subscribe commands
    
    * Add testSubscribeFailed and fix issues
    
    * Test we can run commands on a v3 subscription connection
    
    * Update Sources/Valkey/Subscriptions/ValkeySubscriptions.swift
    
    Co-authored-by: Fabian Fett <fabianfett@apple.com>
    
    * Update Sources/Valkey/Subscriptions/ValkeySubscriptions.swift
    
    Co-authored-by: Fabian Fett <fabianfett@apple.com>
    
    * Update Sources/Valkey/Subscriptions/ValkeySubscriptions.swift
    
    Co-authored-by: Fabian Fett <fabianfett@apple.com>
    
    * Update Sources/Valkey/Subscriptions/ValkeySubscriptions.swift
    
    Co-authored-by: Fabian Fett <fabianfett@apple.com>
    
    * Update Sources/Valkey/Subscriptions/ValkeySubscriptions.swift
    
    Co-authored-by: Fabian Fett <fabianfett@apple.com>
    
    * Update Sources/Valkey/Subscriptions/ValkeySubscriptions.swift
    
    Co-authored-by: Fabian Fett <fabianfett@apple.com>
    
    * PR comments
    
    * Removed creation of empty subscriptionMap entry on closing
    
    ---------
    
    Co-authored-by: Fabian Fett <fabianfett@apple.com>

---
+++ Sources/Valkey/Subscriptions/ValkeyChannelStateMachine.swift
@@ -0,0 +1,141 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-valkey open source project
+//
+// Copyright (c) 2025 Apple Inc. and the swift-valkey project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of swift-valkey project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+/// State machine for a single channel/pattern subscription
+struct ValkeyChannelStateMachine<Value: Identifiable> where Value: AnyObject {
+    enum State {
+        case uninitialized
+        case opening([Value])
+        case active([Value])
+        case closing
+        case closed
+    }
+    var state: State
+
+    init() {
+        self.state = .uninitialized
+    }
+
+    enum AddAction {
+        case doNothing
+        case subscribe
+    }
+    // Add subscription to channel
+    mutating func add(subscription: Value) -> AddAction {
+        switch state {
+        case .uninitialized:
+            self.state = .opening([subscription])
+            return .subscribe
+        case .opening(var subscriptions):
+            subscriptions.append(subscription)
+            self.state = .opening(subscriptions)
+            return .doNothing
+        case .active(var subscriptions):
+            subscriptions.append(subscription)
+            self.state = .active(subscriptions)
+            return .doNothing
+        case .closing:
+            self.state = .opening([subscription])
+            return .subscribe
+        case .closed:
+            preconditionFailure("Closed channels should not be interacted with")
+        }
+    }
+
+    /// Channel has been subscribed to
+    mutating func added() {
+        switch state {
+        case .uninitialized, .closed:
+            preconditionFailure("Inactive channels should not be interacted with")
+        case .opening(let subscriptions):
+            self.state = .active(subscriptions)
+        case .active:
+            break
+        case .closing:
+            preconditionFailure("Cannot add subscription to closing or closed channel")
+        }
+    }
+
+    enum ReceivedMessageAction {
+        case forwardMessage([Value])
+        case doNothing
+    }
+    /// We received a message should we pass it on
+    func receivedMessage() -> ReceivedMessageAction {
+        switch state {
+        case .active(let subscriptions):
+            return .forwardMessage(subscriptions)
+        case .uninitialized, .opening, .closing, .closed:
+            return .doNothing
+        }
+    }
+
+    enum CloseAction {
+        case doNothing
+        case unsubscribe
+    }
+    /// Subscription is being removed from Channel
+    mutating func close(subscription: Value) -> CloseAction {
+        switch self.state {
+        case .uninitialized:
+            self.state = .closing
+            return .doNothing
+        case .opening(var subscriptions):
+            if subscriptions.count == 1 {
+                precondition(subscriptions[0].id == subscription.id, "Cannot be closing a subscription without adding it")
+                self.state = .closing
+                return .unsubscribe
+            } else {
+                guard let index = subscriptions.firstIndex(where: { $0.id == subscription.id }) else {
+                    preconditionFailure("Cannot have added a subscription without adding it")
+                }
+                subscriptions.remove(at: index)
+                self.state = .opening(subscriptions)
+                return .doNothing
+            }
+        case .active(var subscriptions):
+            if subscriptions.count == 1 {
+                precondition(subscriptions[0].id == subscription.id, "Cannot be closing a subscription without adding it")
+                self.state = .closing
+                return .unsubscribe
+            } else {
+                guard let index = subscriptions.firstIndex(where: { $0.id == subscription.id }) else {
+                    preconditionFailure("Cannot have added a subscription without adding it")
+                }
+                subscriptions.remove(at: index)
+                self.state = .active(subscriptions)
+                return .doNothing
+            }
+        case .closing, .closed:
+            preconditionFailure("Removing a subscription from a closing channel is not allowed")
+        }
+    }
+
+    enum ClosedAction {
+        case doNothing
+        case removeChannel
+    }
+    /// We got the unsubscribe push notification
+    mutating func closed() -> ClosedAction {
+        switch self.state {
+        case .uninitialized, .closed:
+            preconditionFailure("Inactive channels should not be interacted with")
+        case .opening, .active:
+            // if we are opening then the channel was subscribed to again after the unsubscribe was initiated
+            return .doNothing
+        case .closing:
+            return .removeChannel
+        }
+    }
+}
---
+++ Sources/Valkey/Subscriptions/ValkeySubscriptionSequence.swift
@@ -0,0 +1,51 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-valkey open source project
+//
+// Copyright (c) 2025 Apple Inc. and the swift-valkey project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of swift-valkey project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+/// Message received from subscription
+public struct ValkeySubscriptionMessage: Sendable, Equatable {
+    public let channel: String
+    public let message: String
+
+    package init(channel: String, message: String) {
+        self.channel = channel
+        self.message = message
+    }
+}
+
+/// Sequence of messages from Valkey subscription
+public struct ValkeySubscriptionSequence: AsyncSequence, Sendable {
+    public typealias Element = ValkeySubscriptionMessage
+
+    typealias BaseAsyncSequence = AsyncThrowingStream<ValkeySubscriptionMessage, Error>
+    typealias Continuation = BaseAsyncSequence.Continuation
+
+    let base: BaseAsyncSequence
+
+    static func makeStream() -> (Self, Self.Continuation) {
+        let (stream, continuation) = BaseAsyncSequence.makeStream()
+        return (.init(base: stream), continuation)
+    }
+
+    public func makeAsyncIterator() -> AsyncIterator {
+        AsyncIterator(base: self.base.makeAsyncIterator())
+    }
+
+    public struct AsyncIterator: AsyncIteratorProtocol {
+        var base: BaseAsyncSequence.AsyncIterator
+
+        public mutating func next() async throws -> Element? {
+            try await self.base.next()
+        }
+    }
+}
