f06eca7cb5f398e15d1f0e3a781f415ae544f1ad
Adam Fowler <adamfowler71@gmail.com>
2025-04-28 18:54:21 +0100

    Convert ConnectionTests

--- Tests/ValkeyTests/ValkeyConnectionTests.swift
+++ Tests/ValkeyTests/ValkeyConnectionTests.swift
@@ -26,7 +26,8 @@ struct ConnectionTests {
     func testConnectionCreationAndGET() async throws {
         let channel = NIOAsyncTestingChannel()
         let logger = Logger(label: "test")
-        let connection = try await ValkeyConnection.setupChannelAndConnect(channel, configuration: .init(respVersion: .v2), logger: logger)
+        let connection = try await ValkeyConnection.setupChannelAndConnect(channel, configuration: .init(), logger: logger)
+        try await channel.processHello()
 
         async let fooResult = connection.get(key: "foo")?.decode(as: String.self)
 
@@ -41,22 +42,22 @@ struct ConnectionTests {
     func testConnectionCreationHelloV3() async throws {
         let channel = NIOAsyncTestingChannel()
         let logger = Logger(label: "test")
-        _ = try await ValkeyConnection.setupChannelAndConnect(channel, configuration: .init(respVersion: .v3), logger: logger)
+        _ = try await ValkeyConnection.setupChannelAndConnect(channel, configuration: .init(), logger: logger)
 
         let outbound = try await channel.waitForOutboundWrite(as: ByteBuffer.self)
-        #expect(outbound == ByteBuffer(string: "*2\r\n$5\r\nHELLO\r\n$1\r\n3\r\n"))
+        #expect(outbound == RESPToken(.array([.bulkString("HELLO"), .bulkString("3")])).base)
     }
 
     @Test
     func testConnectionCreationHelloError() async throws {
         let channel = NIOAsyncTestingChannel()
         let logger = Logger(label: "test")
-        _ = try await ValkeyConnection.setupChannelAndConnect(channel, configuration: .init(respVersion: .v3), logger: logger)
+        _ = try await ValkeyConnection.setupChannelAndConnect(channel, configuration: .init(), logger: logger)
 
         let outbound = try await channel.waitForOutboundWrite(as: ByteBuffer.self)
-        #expect(outbound == ByteBuffer(string: "*2\r\n$5\r\nHELLO\r\n$1\r\n3\r\n"))
+        #expect(outbound == RESPToken(.array([.bulkString("HELLO"), .bulkString("3")])).base)
         await #expect(throws: ValkeyClientError(.commandError, message: "Not supported")) {
-            try await channel.writeInbound(ByteBuffer(string: "!13\r\nNot supported\r\n"))
+            try await channel.writeInbound(RESPToken(.blobError("Not supported")).base)
         }
 
         try await channel.closeFuture.get()
@@ -76,7 +77,10 @@ struct ConnectionTests {
         )
 
         let outbound = try await channel.waitForOutboundWrite(as: ByteBuffer.self)
-        #expect(outbound == ByteBuffer(string: "*5\r\n$5\r\nHELLO\r\n$1\r\n3\r\n$4\r\nAUTH\r\n$4\r\njohn\r\n$5\r\nsmith\r\n"))
+        #expect(
+            outbound
+                == RESPToken(.array([.bulkString("HELLO"), .bulkString("3"), .bulkString("AUTH"), .bulkString("john"), .bulkString("smith")])).base
+        )
     }
 
     @Test
@@ -91,19 +95,20 @@ struct ConnectionTests {
         )
 
         let outbound = try await channel.waitForOutboundWrite(as: ByteBuffer.self)
-        #expect(outbound == ByteBuffer(string: "*4\r\n$5\r\nHELLO\r\n$1\r\n2\r\n$7\r\nSETNAME\r\n$7\r\nTesting\r\n"))
+        #expect(outbound == RESPToken(.array([.bulkString("HELLO"), .bulkString("2"), .bulkString("SETNAME"), .bulkString("Testing")])).base)
     }
 
     @Test
     func testSimpleError() async throws {
         let channel = NIOAsyncTestingChannel()
         let logger = Logger(label: "test")
-        let connection = try await ValkeyConnection.setupChannelAndConnect(channel, configuration: .init(respVersion: .v2), logger: logger)
+        let connection = try await ValkeyConnection.setupChannelAndConnect(channel, configuration: .init(), logger: logger)
+        try await channel.processHello()
 
         async let fooResult = connection.get(key: "foo")
         _ = try await channel.waitForOutboundWrite(as: ByteBuffer.self)
 
-        try await channel.writeInbound(ByteBuffer(string: "-Error!\r\n"))
+        try await channel.writeInbound(RESPToken(.simpleError("Error!")).base)
         do {
             _ = try await fooResult
             Issue.record()
@@ -117,12 +122,13 @@ struct ConnectionTests {
     func testBulkError() async throws {
         let channel = NIOAsyncTestingChannel()
         let logger = Logger(label: "test")
-        let connection = try await ValkeyConnection.setupChannelAndConnect(channel, configuration: .init(respVersion: .v2), logger: logger)
+        let connection = try await ValkeyConnection.setupChannelAndConnect(channel, configuration: .init(), logger: logger)
+        try await channel.processHello()
 
         async let fooResult = connection.get(key: "foo")
         _ = try await channel.waitForOutboundWrite(as: ByteBuffer.self)
 
-        try await channel.writeInbound(ByteBuffer(string: "!10\r\nBulkError!\r\n"))
+        try await channel.writeInbound(RESPToken(.blobError("BulkError!")).base)
         do {
             _ = try await fooResult
             Issue.record()
@@ -136,10 +142,11 @@ struct ConnectionTests {
     func testUnsolicitedErrorToken() async throws {
         let channel = NIOAsyncTestingChannel()
         let logger = Logger(label: "test")
-        _ = try await ValkeyConnection.setupChannelAndConnect(channel, configuration: .init(respVersion: .v2), logger: logger)
+        _ = try await ValkeyConnection.setupChannelAndConnect(channel, configuration: .init(), logger: logger)
+        try await channel.processHello()
 
         await #expect(throws: ValkeyClientError(.unsolicitedToken, message: "Received an error token without having sent a command")) {
-            try await channel.writeInbound(ByteBuffer(string: "-Error!\r\n"))
+            try await channel.writeInbound(RESPToken(.simpleError("Error!")).base)
         }
         try await channel.closeFuture.get()
     }
@@ -148,10 +155,11 @@ struct ConnectionTests {
     func testUnsolicitedToken() async throws {
         let channel = NIOAsyncTestingChannel()
         let logger = Logger(label: "test")
-        _ = try await ValkeyConnection.setupChannelAndConnect(channel, configuration: .init(respVersion: .v2), logger: logger)
+        _ = try await ValkeyConnection.setupChannelAndConnect(channel, configuration: .init(), logger: logger)
+        try await channel.processHello()
 
         await #expect(throws: ValkeyClientError(.unsolicitedToken, message: "Received a token without having sent a command")) {
-            try await channel.writeInbound(ByteBuffer(string: "$3\r\nBar\r\n"))
+            try await channel.writeInbound(RESPToken(.bulkString("Bar")).base)
         }
         try await channel.closeFuture.get()
     }
