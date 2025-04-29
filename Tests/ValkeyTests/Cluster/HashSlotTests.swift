5e5b95e9b97bfc147f80491349919c1e4b884c34
Fabian Fett <fabianfett@apple.com>
2025-04-29 10:31:28 +0200

    Add HashSlotShardMap

--- Tests/ValkeyTests/Cluster/HashSlotTests.swift
+++ Tests/ValkeyTests/Cluster/HashSlotTests.swift
@@ -1,12 +1,12 @@
 //===----------------------------------------------------------------------===//
 //
-// This source file is part of the RediStack open source project
+// This source file is part of the swift-valkey project
 //
-// Copyright (c) 2023 Apple Inc. and the RediStack project authors
+// Copyright (c) 2025 the swift-valkey authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
-// See CONTRIBUTORS.txt for the list of RediStack project authors
+// See swift-valkey/CONTRIBUTORS.txt for the list of swift-valkey authors
 //
 // SPDX-License-Identifier: Apache-2.0
 //
