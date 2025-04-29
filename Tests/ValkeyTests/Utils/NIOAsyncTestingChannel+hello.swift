bb14ac2e45672c10babb9bda60917aa720294581
Adam Fowler <adamfowler71@gmail.com>
2025-04-29 09:17:04 +0100

    Cleanup tests (#53)
    * Convert ConnectionTests
    
    * Cleanup testSubscribe
    
    * Subscription tests

---
+++ Tests/ValkeyTests/Utils/NIOAsyncTestingChannel+hello.swift
@@ -0,0 +1,39 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-valkey open source project
+//
+// Copyright (c) 2025 Apple Inc. and the swift-valkey project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of swift-valkey project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import NIOCore
+import NIOEmbedded
+import Testing
+
+@testable import Valkey
+
+extension NIOAsyncTestingChannel {
+    func processHello(version: ValkeyClientConfiguration.RESPVersion = .v3) async throws {
+        let hello = try await self.waitForOutboundWrite(as: ByteBuffer.self)
+        #expect(hello == RESPToken(.array([.bulkString("HELLO"), .bulkString("\(version.rawValue)")])).base)
+        try await self.writeInbound(
+            RESPToken(
+                .map([
+                    .bulkString("server"): .bulkString("valkey"),
+                    .bulkString("version"): .bulkString("8.0.2"),
+                    .bulkString("proto"): .number(3),
+                    .bulkString("id"): .number(1117),
+                    .bulkString("mode"): .bulkString("standalone"),
+                    .bulkString("role"): .bulkString("master"),
+                    .bulkString("modules"): .array([]),
+                ])
+            ).base
+        )
+    }
+}
