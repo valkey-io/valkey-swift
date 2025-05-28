c5a0095999b357220f880878c9b6e9e1a4dbc634
Fabian Fett <fabianfett@apple.com>
2025-05-28 11:28:46 +0200

    Enhanced ValkeyClusterDiscovery Documentation

--- Sources/Valkey/Cluster/ValkeyClusterDiscovery.swift
+++ Sources/Valkey/Cluster/ValkeyClusterDiscovery.swift
@@ -14,7 +14,7 @@
 
 /// Allows the cluster client to initially find at least one node in the cluster or find the
 /// nodes again if connection to the has been lost.
-public protocol ValkeyClusterDiscovery {
+public protocol ValkeyNodeDiscovery: Sendable {
     /// A type that describes a single node in a valkey cluster
     associatedtype NodeDescription: ValkeyNodeDescriptionProtocol
 
@@ -22,26 +22,32 @@ public protocol ValkeyClusterDiscovery {
     associatedtype NodeDescriptions: Collection<NodeDescription>
 
     /// Lookup all the nodes within the cluster.
+    ///
+    /// - Returns: A collection of `NodeDescription` objects representing the nodes in the cluster.
+    /// - Throws: An error if the lookup fails.
     func lookupNodes() async throws -> NodeDescriptions
 }
 
-/// A description of a single node that is part of a valkey cluster
-public protocol ValkeyNodeDescriptionProtocol: Sendable, Equatable {
-    /// The node's host name.
-    var host: String? { get }
-    /// The node's ip address.
-    var ip: String? { get }
-    /// The nodes endpoint. This should normally be the ``host`` if the node has a routable hostname.
-    /// Otherwise it is the ``ip``. This property is used to create connections to the node.
-    var endpoint: String { get }
-    /// The node's redis port
-    var port: Int { get }
-    /// Defines if TLS shall be used to create a connection to the node
-    var useTLS: Bool { get }
-}
-
-public struct ValkeyStaticClusterDiscovery: ValkeyClusterDiscovery {
-
+/// A concrete implementation of ``ValkeyNodeDiscovery`` that provides a static, pre-configured
+/// list of nodes in a Valkey cluster.
+///
+/// Use this implementation when you have a fixed set of nodes that don't change frequently.
+/// It maintains an array of node descriptions that is provided at initialization time and
+/// returns this static list when ``lookupNodes()`` is called.
+///
+/// This type conforms to `ExpressibleByArrayLiteral`, allowing you to create an instance
+/// directly using an array literal syntax.
+///
+/// Example:
+/// ```swift
+/// // Using ExpressibleByArrayLiteral conformance for more concise initialization
+/// let literalDiscovery: ValkeyStaticNodeDiscovery = [
+///     .init(host: "replica1.valkey.io", port: 10600, useTLS: false),
+///     .init(ip: "192.168.12.1", port: 10600, useTLS: true)
+/// ]
+/// ```
+public struct ValkeyStaticNodeDiscovery: ValkeyNodeDiscovery {
+    /// A description of a single node in the Valkey cluster.
     public struct NodeDescription: ValkeyNodeDescriptionProtocol {
         public var host: String?
         public var ip: String?
@@ -49,7 +55,14 @@ public struct ValkeyStaticClusterDiscovery: ValkeyClusterDiscovery {
         public var port: Int
         public var useTLS: Bool
 
-        public init(host: String, ip: String? = nil, port: Int = 5678, useTLS: Bool = true) {
+        /// Initializes a `NodeDescription` with a host and optional IP.
+        ///
+        /// - Parameters:
+        ///   - host: The host name of the node.
+        ///   - ip: The optional IP address of the node.
+        ///   - port: The port number the node listens on (default is 6379).
+        ///   - useTLS: A boolean indicating whether TLS should be used (default is true).
+        public init(host: String, ip: String? = nil, port: Int = 6379, useTLS: Bool = true) {
             self.host = host
             self.ip = ip
             self.endpoint = host
@@ -57,7 +70,13 @@ public struct ValkeyStaticClusterDiscovery: ValkeyClusterDiscovery {
             self.useTLS = useTLS
         }
 
-        public init(ip: String, port: Int = 5678, useTLS: Bool = false) {
+        /// Initializes a `NodeDescription` with an IP address.
+        ///
+        /// - Parameters:
+        ///   - ip: The IP address of the node.
+        ///   - port: The port number the node listens on (default is 6379).
+        ///   - useTLS: A boolean indicating whether TLS should be used (default is false).
+        public init(ip: String, port: Int = 6379, useTLS: Bool = false) {
             self.host = nil
             self.ip = ip
             self.endpoint = ip
@@ -68,11 +87,27 @@ public struct ValkeyStaticClusterDiscovery: ValkeyClusterDiscovery {
 
     private var nodes: [NodeDescription]
 
+    /// Initializes a ``ValkeyStaticClusterDiscovery`` with a list of nodes.
+    ///
+    /// - Parameter nodes: An array of ``ValkeyStaticNodeDiscovery/NodeDescription`` objects representing the nodes in the cluster.
     public init(_ nodes: [NodeDescription]) {
         self.nodes = nodes
     }
 
+    /// Lookup all the nodes within the cluster.
+    ///
+    /// - Returns: An array of ``ValkeyStaticNodeDiscovery/NodeDescription`` objects representing the nodes in the cluster.
     public func lookupNodes() -> [NodeDescription] {
         self.nodes
+    }
+}
+
+// MARK: - ExpressibleByArrayLiteral Conformance
+
+extension ValkeyStaticNodeDiscovery: ExpressibleByArrayLiteral {
+    public typealias ArrayLiteralElement = NodeDescription
+
+    public init(arrayLiteral elements: ArrayLiteralElement...) {
+        self.nodes = elements
     }
 }
