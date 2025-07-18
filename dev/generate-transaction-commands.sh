7fcc83a72a6df967d917963f3797ac1e93223895
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-07-18 14:04:03 -0400

    spelling: interval
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- dev/generate-transaction-commands.sh
+++ dev/generate-transaction-commands.sh
@@ -44,7 +44,7 @@ echo ""
 echo "@available(valkeySwift 1.0, *)"
 echo "extension ValkeyConnection {"
 # note:
-# - widening the inverval below (eg. going from {1..15} to {1..25}) is Semver minor
+# - widening the interval below (eg. going from {1..15} to {1..25}) is Semver minor
 # - narrowing the interval below is SemVer _MAJOR_!
 for n in {1..10}; do
     genWithoutContextParameter "$n"
