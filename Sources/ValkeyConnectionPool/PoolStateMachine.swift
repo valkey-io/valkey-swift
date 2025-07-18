37a8e09bfab25cb731b80f2242f6872b2550e43f
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-07-18 14:33:27 -0400

    spelling: for
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- Sources/ValkeyConnectionPool/PoolStateMachine.swift
+++ Sources/ValkeyConnectionPool/PoolStateMachine.swift
@@ -17,7 +17,7 @@ struct PoolConfiguration: Sendable {
     @usableFromInline
     var minimumConnectionCount: Int = 0
 
-    /// The maximum number of connections to for this pool, to be preserved.
+    /// The maximum number of connections for this pool, to be preserved.
     @usableFromInline
     var maximumConnectionSoftLimit: Int = 10
 
