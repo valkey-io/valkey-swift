b0e08b189f4b24fa5e7c23f275c36f5a541bd484
Fabian Fett <fabianfett@apple.com>
2025-07-22 18:36:38 +0200

    Split up benchmarks even more
    Signed-off-by: Fabian Fett <fabianfett@apple.com>

--- Benchmarks/ValkeyBenchmarks/ValkeyBenchmarks.swift
+++ Benchmarks/ValkeyBenchmarks/ValkeyBenchmarks.swift
@@ -19,32 +19,13 @@ import NIOCore
 import NIOPosix
 import Valkey
 
-let defaultMetrics: [BenchmarkMetric] =
-    // There is no point comparing wallClock, cpuTotal or throughput on CI as they are too inconsistent
-    ProcessInfo.processInfo.environment["CI"] != nil
-    ? [
-        .instructions,
-        .mallocCountTotal,
-    ]
-    : [
-        .wallClock,
-        .cpuTotal,
-        .instructions,
-        .mallocCountTotal,
-        .throughput,
-    ]
-
 let benchmarks: @Sendable () -> Void = {
 
 
     if #available(valkeySwift 1.0, *) {
-        makeConnectionGETBenchmark()
+        connectionBenchmarks()
 
-        makeClientGETSequentialBenchmark()
-
-        makeClient20ConcurrentGETBenchmark()
-
-        makeClient50Concurrent20ConnectionGETBenchmark()
+        clientBenchmarks()
 
         Benchmark("ValkeyCommandEncoder – Simple GET", configuration: .init(metrics: defaultMetrics, scalingFactor: .kilo)) { benchmark in
             let command = GET("foo")
@@ -95,50 +76,6 @@ let benchmarks: @Sendable () -> Void = {
             benchmark.startMeasurement()
             for _ in benchmark.scaledIterations {
                 blackHole(HashSlot(key: key))
-            }
-        }
-    }
-}
-
-protocol BenchmarkCommandHandler {
-    func handle(command: RESPToken.Value, parameters: RESPToken.Array.Iterator, write: (ByteBuffer) -> Void)
-}
-
-final class ValkeyServerChannelHandler<Handler: BenchmarkCommandHandler>: ChannelInboundHandler {
-
-    typealias InboundIn = ByteBuffer
-    typealias OutboundOut = ByteBuffer
-
-    private var decoder = NIOSingleStepByteToMessageProcessor(RESPTokenDecoder())
-    private let helloCommand = RESPToken.Value.bulkString(ByteBuffer(string: "HELLO"))
-    private let helloResponse = ByteBuffer(string: "%1\r\n+server\r\n+fake\r\n")
-    private let commandHandler: Handler
-
-    init(commandHandler: Handler) {
-        self.commandHandler = commandHandler
-    }
-
-    func channelRead(context: ChannelHandlerContext, data: NIOAny) {
-        try! self.decoder.process(buffer: self.unwrapInboundIn(data)) { token in
-            self.handleToken(context: context, token: token)
-        }
-    }
-
-    func handleToken(context: ChannelHandlerContext, token: RESPToken) {
-        guard case .array(let array) = token.value else {
-            fatalError()
-        }
-        var iterator = array.makeIterator()
-        guard let command = iterator.next()?.value else {
-            fatalError()
-        }
-        switch command {
-        case helloCommand:
-            context.writeAndFlush(self.wrapOutboundOut(helloResponse), promise: nil)
-
-        default:
-            commandHandler.handle(command: command, parameters: iterator) {
-                context.writeAndFlush(self.wrapOutboundOut($0), promise: nil)
             }
         }
     }
