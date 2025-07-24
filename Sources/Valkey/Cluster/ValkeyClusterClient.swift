d33aa4e6651c295b6d149ee1a3846278771981d4
Adam Fowler <adamfowler71@gmail.com>
2025-07-24 21:59:11 +0100

    Update Sources/Valkey/Cluster/ValkeyClusterClient.swift
    Co-authored-by: Fabian Fett <fabianfett@apple.com>
    Signed-off-by: Adam Fowler <adamfowler71@gmail.com>

--- Sources/Valkey/Cluster/ValkeyClusterClient.swift
+++ Sources/Valkey/Cluster/ValkeyClusterClient.swift
@@ -167,7 +167,7 @@ public final class ValkeyClusterClient: Sendable {
                 guard let errorMessage = error.message, let movedError = ValkeyMovedError(errorMessage) else {
                     throw error
                 }
-                self.logger.trace("Received move error \(movedError)")
+                self.logger.trace("Received move error", metadata: ["error": "\(movedError)"])
                 clientSelector = { try await self.client(for: movedError) }
             }
         }
