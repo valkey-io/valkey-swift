2512ee20410b19a59c2275a117a79084f672c471
Adam Fowler <adamfowler71@gmail.com>
2025-08-21 11:57:38 +0200

    Alphabetical
    Signed-off-by: Adam Fowler <adamfowler71@gmail.com>

--- Sources/Valkey/Connection/ValkeyServerAddress.swift
+++ Sources/Valkey/Connection/ValkeyServerAddress.swift
@@ -18,8 +18,8 @@ import NIOCore
 public struct ValkeyServerAddress: Sendable, Equatable {
     enum _Internal: Equatable {
         case hostname(_ host: String, port: Int)
-        case unixDomainSocket(path: String)
         case socketAddress(SocketAddress)
+        case unixDomainSocket(path: String)
     }
 
     let value: _Internal
@@ -27,10 +27,10 @@ public struct ValkeyServerAddress: Sendable, Equatable {
         self.value = value
     }
 
-    // raw socket address
-    public static func socketAddress(_ address: SocketAddress) -> Self { .init(.socketAddress(address)) }
     // Address defined by host and port. If using raw IP address for host name it is preferable to use ``ValkeyServerAddress/socketAddress(_:)``.
     public static func hostname(_ host: String, port: Int = 6379) -> Self { .init(.hostname(host, port: port)) }
+    // raw socket address
+    public static func socketAddress(_ address: SocketAddress) -> Self { .init(.socketAddress(address)) }
     // Address defined by unxi domain socket
     public static func unixDomainSocket(path: String) -> Self { .init(.unixDomainSocket(path: path)) }
 }
