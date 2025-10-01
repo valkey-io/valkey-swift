8e99b942a03e1b6b249ae57d07dc65b4c06be395
Adam Fowler <adamfowler71@gmail.com>
2025-10-01 14:23:08 +0100

    Add existential execute to ValkeyClientProtocol (#235)
    * Add existential execute to ValkeyClientProtocol
    
    Signed-off-by: Adam Fowler <adamfowler71@gmail.com>
    
    * remove `@inlinable` from protocol
    
    Signed-off-by: Adam Fowler <adamfowler71@gmail.com>
    
    ---------
    
    Signed-off-by: Adam Fowler <adamfowler71@gmail.com>

--- Sources/Valkey/ValkeyClientProtocol.swift
+++ Sources/Valkey/ValkeyClientProtocol.swift
@@ -7,9 +7,23 @@
 //
 /// A type that provides the ability to send a Valkey command and get a response.
 @available(valkeySwift 1.0, *)
-public protocol ValkeyClientProtocol {
+public protocol ValkeyClientProtocol: Sendable {
     /// Send RESP command to Valkey connection
     /// - Parameter command: ValkeyCommand structure
     /// - Returns: The command response as defined in the ValkeyCommand
     func execute<Command: ValkeyCommand>(_ command: Command) async throws -> Command.Response
+
+    /// Pipeline a series of commands to Valkey connection
+    ///
+    /// Once all the responses for the commands have been received the function returns
+    /// an array of RESPToken Results, one for each command.
+    ///
+    /// This is an alternative version of the pipelining function ``ValkeyClient/execute(_:)->(_,_)``
+    /// that allows for a collection of ValkeyCommands. It provides more flexibility but
+    /// is more expensive to run and the command responses are returned as ``RESPToken``
+    /// instead of the response type for the command.
+    ///
+    /// - Parameter commands: Collection of ValkeyCommands
+    /// - Returns: Array holding the RESPToken responses of all the commands
+    func execute(_ commands: [any ValkeyCommand]) async -> sending [Result<RESPToken, Error>]
 }
