874a3f41c28d213c2a1052d22516cb3cb7446a9e
Adam Fowler <adamfowler71@gmail.com>
2025-12-29 18:45:34 +0000

    Call setPrimary for all server address types
    Signed-off-by: Adam Fowler <adamfowler71@gmail.com>

--- Sources/Valkey/ValkeyClient.swift
+++ Sources/Valkey/ValkeyClient.swift
@@ -85,20 +85,16 @@ public final class ValkeyClient: Sendable {
         self.runningAtomic = .init(false)
         self.stateMachine = .init(.init(poolFactory: self.nodeClientFactory, configuration: connectionFactory.configuration))
         (self.actionStream, self.actionStreamContinuation) = AsyncStream.makeStream(of: RunAction.self)
-        switch address.value {
-        case .hostname(let host, let port):
-            let action = self.stateMachine.withLock { $0.setPrimary(.hostname(host, port: port)) }
-            switch action {
-            case .runNode(let client):
-                self.queueAction(.runNodeClient(client))
-            case .runNodeAndFindReplicas(let client):
-                self.queueAction(.runNodeClient(client))
-                self.queueAction(.runRole(client))
-            case .findReplicas, .doNothing:
-                preconditionFailure("First time you call setPrimary it should always return runNodeAndFindReplicas")
-            }
-        case .unixDomainSocket:
-            fatalError("Currently unsupported")
+
+        let action = self.stateMachine.withLock { $0.setPrimary(address) }
+        switch action {
+        case .runNode(let client):
+            self.queueAction(.runNodeClient(client))
+        case .runNodeAndFindReplicas(let client):
+            self.queueAction(.runNodeClient(client))
+            self.queueAction(.runRole(client))
+        case .findReplicas, .doNothing:
+            preconditionFailure("First time you call setPrimary it should return runNodeAndFindReplicas or runNode")
         }
     }
 }
