7a164d53a0b9958aa667e120a7406546f4825633
Adam Fowler <adamfowler71@gmail.com>
2026-01-04 11:48:27 +0000

    Add test for standalone primary selection
    Signed-off-by: Adam Fowler <adamfowler71@gmail.com>
    
    Update test
    
    Signed-off-by: Adam Fowler <adamfowler71@gmail.com>
    
    Enable client redirect (not handling it yet)
    
    Signed-off-by: Adam Fowler <adamfowler71@gmail.com>

--- Sources/Valkey/Connection/ValkeyConnectionConfiguration.swift
+++ Sources/Valkey/Connection/ValkeyConnectionConfiguration.swift
@@ -124,6 +124,13 @@ public struct ValkeyConnectionConfiguration: Sendable {
     /// The number of Valkey Database
     public var databaseNumber: Int = 0
 
+    /// Enable client redirect capability
+    ///
+    /// Valkey 8.0 introduced a new command CLIENT CAPA to indicate client capabilities. It
+    /// currently only supports one capability `redirect`. This indicates the client is
+    /// capable of handling redirect messages from replica nodes back to the primary
+    var enableClientRedirect: Bool
+
     #if DistributedTracingSupport
     /// The distributed tracing configuration to use for this connection.
     /// Defaults to using the globally bootstrapped tracer with OpenTelemetry semantic conventions.
@@ -159,6 +166,7 @@ public struct ValkeyConnectionConfiguration: Sendable {
         self.clientName = clientName
         self.readOnly = readOnly
         self.databaseNumber = databaseNumber
+        self.enableClientRedirect = true
     }
 }
 
