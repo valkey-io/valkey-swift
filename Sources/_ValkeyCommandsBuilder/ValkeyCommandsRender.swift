159713ec0e04504c06d9a05886cd552228010cce
Adam Fowler <adamfowler71@gmail.com>
2026-01-12 19:05:24 +0000

    Copyright (c) 2025-2026 command files
    Signed-off-by: Adam Fowler <adamfowler71@gmail.com>

--- Sources/_ValkeyCommandsBuilder/ValkeyCommandsRender.swift
+++ Sources/_ValkeyCommandsBuilder/ValkeyCommandsRender.swift
@@ -105,7 +105,7 @@ func renderValkeyCommands(_ commands: [String: ValkeyCommand], fullCommandList: 
     var string = """
         //
         // This source file is part of the valkey-swift project
-        // Copyright (c) 2025 the valkey-swift project authors
+        // Copyright (c) 2025-2026 the valkey-swift project authors
         //
         // See LICENSE.txt for license information
         // SPDX-License-Identifier: Apache-2.0
