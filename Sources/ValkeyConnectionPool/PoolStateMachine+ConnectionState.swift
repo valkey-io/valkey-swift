541eb18fb4d3bcc64f9431a9441a1306973d70c3
Adam Fowler <adamfowler71@gmail.com>
2026-03-04 07:21:10 +0000

    RescheduleIdleTimer change from postgres connection pool (#356)

--- Sources/ValkeyConnectionPool/PoolStateMachine+ConnectionState.swift
+++ Sources/ValkeyConnectionPool/PoolStateMachine+ConnectionState.swift
@@ -321,6 +321,26 @@ extension PoolStateMachine {
             }
         }
 
+        @inlinable
+        mutating func rescheduleIdleTimer() -> ConnectionTimer? {
+            switch self.state {
+            case .backingOff, .starting:
+                preconditionFailure("Invalid state: \(self.state)")
+
+            case .idle(let connection, let maxStreams, let keepAlive, .some):
+                let idleTimerState = self._nextTimer()
+                let idleTimer = ConnectionTimer(timerID: idleTimerState.timerID, connectionID: self.id, usecase: .idleTimeout)
+                self.state = .idle(connection, maxStreams: maxStreams, keepAlive: keepAlive, idleTimer: idleTimerState)
+                return idleTimer
+
+            case .idle(_, _, _, .none):
+                return nil
+
+            case .leased, .closing, .closed:
+                return nil
+            }
+        }
+
         /// The connection failed to start
         @inlinable
         mutating func failedToConnect() -> ConnectionTimer {
